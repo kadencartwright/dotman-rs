7908f20061c60ca7b3c96d4f8eb689462a479083
kadencartwright <cartwrightkaden@gmail.com>
2023-01-02 15:29:25 -0700

    updated for optional config file

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,9 @@
-use clap::{Parser, Subcommand};
+use clap::{arg, Parser, Subcommand};
 
-use crate::mapping_definitions::define_mappings;
+use crate::{
+    file_mapping::FileMapping,
+    mapping_definitions::{define_mappings, read_mappings_config_file},
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,22 +17,33 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// adds the defined files to the config repo
-    CopyToVersionControl,
+    CopyToVersionControl {
+        #[arg(long, short)]
+        file: Option<String>,
+    },
     /// backs up original config files, then overwrites them with symlinks from the config repo
-    LinkFromVersionControl,
+    LinkFromVersionControl {
+        #[arg(long, short)]
+        file: Option<String>,
+    },
 }
 pub fn process_command() {
     let cli = Cli::parse();
-    let mut mappings = define_mappings();
 
     // map the cli enum to it's corresponding action
     match &cli.command {
-        Commands::CopyToVersionControl => mappings
+        Commands::CopyToVersionControl { file } => get_mappings(file)
             .iter_mut()
             .for_each(|mapping| mapping.copy_to_version_control()),
 
-        Commands::LinkFromVersionControl => mappings
+        Commands::LinkFromVersionControl { file } => get_mappings(file)
             .iter_mut()
             .for_each(|mapping| mapping.link_from_version_control()),
     }
+}
+fn get_mappings(maybe_file: &Option<String>) -> Vec<FileMapping> {
+    return match maybe_file {
+        Some(path) => read_mappings_config_file(path),
+        None => define_mappings(),
+    };
 }
