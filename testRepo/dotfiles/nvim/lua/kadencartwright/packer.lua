3a25dbf5cdc1b7a38458ebb11eee24b9d20c423b
Kaden Cartwright <kaden.cartwright@shellenergy.com>
2023-05-04 07:51:22 -0600

    updated nvim packer plugins

--- testRepo/dotfiles/nvim/lua/kadencartwright/packer.lua
+++ testRepo/dotfiles/nvim/lua/kadencartwright/packer.lua
@@ -10,11 +10,11 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
     use('navarasu/onedark.nvim')
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
+    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
@@ -23,24 +23,33 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
         }
     }
-    use ( 'tpope/vim-dadbod')
-    use ( 'kristijanhusak/vim-dadbod-ui' )
-    use  ('styled-components/vim-styled-components')
+    use('tpope/vim-dadbod')
+    use('kristijanhusak/vim-dadbod-ui')
+    use('styled-components/vim-styled-components')
+    use {
+        'MunifTanjim/prettier.nvim',
+        requires = {
+            'jose-elias-alvarez/null-ls.nvim'
+        }
+    }
+    use('towolf/vim-helm')
+    use('windwp/nvim-ts-autotag')
+    use('windwp/nvim-autopairs')
 end)
