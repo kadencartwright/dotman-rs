230d7c2291c2c7d1da071a1ee9d09201b4bbab6d
Kaden Cartwright <cartwrightkaden@gmail.com>
2023-09-08 12:12:14 -0600

    added c-w remap for nvim

--- testRepo/dotfiles/nvim/lua/kadencartwright/remap.lua
+++ testRepo/dotfiles/nvim/lua/kadencartwright/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>pv",vim.cmd.Ex)
-
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", "<c-w>", { noremap = true })
